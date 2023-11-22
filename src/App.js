6879f82a721bd22fd33f08541a4f220b7e5d50e5
pavan <pavannunugoppula1@gmail.com>
2023-11-22 08:20:56 +0000

    renderings

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  renderAuthButton = () => {
+    const {isLoggedIn} = this.state
+    if (isLoggedIn === true) {
+      return <button type="button">Logout</button>
+    }
+
+    return <button type="button">Login</button>
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+        {this.renderAuthButton()}
+      </div>
+    )
+  }
+}
+
+export default App
